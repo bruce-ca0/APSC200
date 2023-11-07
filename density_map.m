bac7d3f151d48f16916b5858f6d6bd9ac6e79120
jordancorbett <117589944+jordancorbett@users.noreply.github.com>
2023-11-07 17:48:27 -0500

    Add files via upload

---
+++ density_map.m
@@ -0,0 +1,14 @@
+function d = density_map(x,y,t)
+%{
+Let "n" be the length of the grid.
+
+--- Inputs ---
+x, y: n by n meshgrid of x and y coordinates
+   t: time  
+
+--- Outputs ---
+d: n by n meshgrid of densities.  All densities must be on [0,inf)
+%}
+
+
+end
